43f8a44ae2556f16da933d4f94f9747c8cdf1cb9
Ho Yeon CHAE <comfynumb@hufs.ac.kr>
2024-12-15 11:42:33 +0900

    <config> : Fundamental Project Setting

---
+++ src/main/java/com/hhp/precourse/FundamentalCrudApplication.java
@@ -0,0 +1,13 @@
+package com.hhp.precourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentalCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hhp/precourse/FundamentalCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhp.precourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentalCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
